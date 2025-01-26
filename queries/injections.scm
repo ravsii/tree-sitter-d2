772b81db7a01b538ff1c80c4e05b140cd4ed9b9f
Alex <naplle58@gmail.com>
2025-01-26 21:39:08 +0300

    feat: empty codeblock as markdown, underscore fixes

--- queries/injections.scm
+++ queries/injections.scm
@@ -3,7 +3,12 @@
   (codeblock_content) @injection.content)
 
 ((label_codeblock
-  (codeblock_language) @language
   (codeblock_content) @injection.content)
-  (#eq? @language "md")
+  (#set! injection.language "markdown"))
+
+((label_codeblock
+  (codeblock_language) @injection.language
+  (codeblock_content) @injection.content)
+  (#any-of? @injection.language
+    "md" "")
   (#set! injection.language "markdown"))
