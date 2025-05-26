09b38b5b68aa226b20d313ea3a0b27ffff8f8da4
Alex <naplle58@gmail.com>
2025-05-26 22:08:42 +0300

    fix(nvim): main branch is now usable

--- lua/tree-sitter-d2/init.lua
+++ lua/tree-sitter-d2/init.lua
@@ -1,37 +1,35 @@
 local M = {}
 
 function M.setup()
-  local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+  local ok, nvim_parsers = pcall(require, "nvim-treesitter.parsers")
   if not ok then
     vim.notify("tree-sitter-d2: can't import nvim-treesitter.parsers", vim.log.levels.WARN)
     return
   end
 
-  ---@module "nvim-treesitter.parsers"
-  ---@type table<string, ParserInfo>
-  local parser_config = parsers.get_parser_configs()
+  --- @type ParserInfo[]
+  local parsers
+
+  if type(nvim_parsers.get_parser_configs) == "function" then
+    parsers = nvim_parsers.get_parser_configs()
+  else
+    parsers = nvim_parsers
+  end
+
+  if type(parsers.d2) == "table" then -- already present
+    return
+  end
 
   local file = debug.getinfo(1).source:match("@(.*/)")
   local plugin_dir = vim.fn.fnamemodify(file, ":p:h:h:h")
-  parser_config.d2 = {
+  parsers.d2 = {
     install_info = {
       url = plugin_dir,
       files = { "src/parser.c" },
       branch = "main",
     },
-    maintainers = { "@ravsii" },
-    filetype = "d2",
+    requires_generate_from_grammar = true,
   }
-
-  vim.filetype.add({
-    extension = {
-      d2 = function()
-        return "d2", function(bufnr)
-          vim.bo[bufnr].commentstring = "# %s"
-        end
-      end,
-    },
-  })
 end
 
 return M
